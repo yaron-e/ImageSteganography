577bf977456c3734f6bb07bab9b4ed08bc1dd9d1
Yaron Eidelman <dogkiller-e@hotmail.com>
2017-09-10 12:39:19 -0400

    Fully working project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,73 +9,110 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(String [] args) throws IOException {
-		String imageFile = "C:/Users/YARON/Pictures/black.png";
+		String imageFile = "C:/Users/YARON/Pictures/img.jpg";
 		//createFile("file1.txt", "Hello world.");
-		
-		writeToImage(imageFile, "h");
-		
 
-		/*String hello = "hello";
-		byte[] b = hello.getBytes();
+		String pathToNewImage = writeToImage(imageFile, "hello world, i am you master. bwhaaaaaahaaaaahaaa");
+		String msg = readMsgFromFile(pathToNewImage);
+		System.out.println(msg);
 
-		for (int i = 0; i < 1; i++) {
-			System.out.println(b[i]);
-			byte curr = b[i];
-			for (int j = 7; j >= 0; j --) {
-				int bit = ((curr & 0xff) >> j) & 0x1;
-				if (bit == 0 )
-					System.out.print(0);
-				else 
-					System.out.print(1);
-			}
-			System.out.println();
-		}*/
 	}
-	
-	public static void writeToImage(final String path, String msg ) throws IOException {
-		/*BufferedImage originalImage = ImageIO.read(new File(path));
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write( originalImage, "jpg", baos );
-		baos.flush();
-		byte[] imageInByte = Base64.getEncoder(baos.toString("UTF-8"));//.getBytes("UTF-8");//toByteArray();
-		baos.close();
-		*/
+
+	private static String readMsgFromFile(String pathToNewImage) throws IOException {
+		FileInputStream fis = new FileInputStream(new File(pathToNewImage));	
 		
+		byte[] b = null;
+		int read = 0; 
+		int size = 0;
+		read = fis.read(new byte[107]);
+		for (int i = 108; (read = fis.read()) != -1; i++)  {
+			if (i <= 107) {//Header
+				//Do nothing
+			} else if (i > 107 && i < 112) {//Message size
+				size = fis.read() ;
+				size += fis.read() << 8;
+				size += fis.read() << 16;
+				size += fis.read() << 24;
+				
+				System.out.println("Size = " + size);
+				i = 111;
+			} else if (i < size*8 + 112 && i >= 112) {//Message body
+				if (b == null)
+					b = new byte[size];
+
+				
+				for (int j = 0; j < 8; j++) {
+					b[(i-112)/8]= (byte) (b[(i-112)/8] << 1);
+					b[(i-112)/8] = (byte) ((read & 0x1) | ((byte)b[(i-112)/8]));
+					if (j != 7)
+						read = fis.read();
+				}
+				//System.out.println(new String(b[(i-112)/8]+""));
+				i +=7;
+			} else {
+				break;
+				//Don't do anything
+			}
+		}
+		fis.close();
+		
+		return new String(b);
+	}
+
+	public static String writeToImage(final String path, String msg ) throws IOException {
 		byte [] msgB = msg.getBytes("UTF-8");
 		File f = new File(new File(path).getParent() + "/newFile.png");
 		f.createNewFile();
 		FileInputStream fis = new FileInputStream(new File(path));	
-		System.err.println(f.getPath());
-		
+		//System.err.println(f.getPath());
+
 		FileOutputStream fos = new FileOutputStream(f, false);
 		int read = 0;		
 		int msgSize = msgB.length;
+		System.out.println("Real size = " + msgSize);
 		for (int i = 0;(read = fis.read()) != -1; i++) {
-			if (i < msgSize*8 + 107 && i > 107) {
+			if (i > 107 && i < 112) {//Store the message size
+				byte [] b = allocateBytesForMsgSize(msgSize);
+				fos.write(b);
+				i = 111;
+				fis.read(new byte[3]);
+			} else if (i < msgSize*8 + 112 && i >= 112) {
 				int x = getByteFromMsg(msgB, i,read);
 				fos.write(x);
-				System.out.print(read + " " + x);
+				//System.out.print(read + " " + x);
 			} else {
 				fos.write(read);
-				System.out.print(read);
+				//System.out.print(read);
 			}
 			fos.flush();
-			System.out.println();
+			//System.out.println();
 		}
 
-		
+
 		fos.close();
 		fis.close();
-
+		
+		return f.getPath();
 	}
-	
+
+	private static byte[] allocateBytesForMsgSize(int msgSize) {
+		byte [] b= new byte[4];
+		
+		b[0] = (byte) (0xFF & msgSize);
+		b[1] = (byte) ((byte) (0xFF & msgSize) >> 8);
+		b[2] = (byte) ((byte) (0xFFF & msgSize) >> 16);
+		b[3] = (byte) ((byte) (0xFFFF & msgSize) >> 24);
+		
+		return b;
+	}
+
 	public static int getByteFromMsg(byte [] msg, int i, int image) {
-		 byte b = msg[(i-107)/8];
-		 int msb = ((b & 0xFF) >> 7) & 0x1;
-		 msg[(i-107)/8] = (byte) (b << 1);
-		 
-		 image = (((image & 0x1) & 0) | msb ) | (image & 0xFE);
-		 
+		byte b = msg[(i-112)/8];
+		int msb = ((b & 0xFF) >> 7) & 0x1;
+		msg[(i-112)/8] = (byte) (b << 1);
+
+		image = (((image & 0x1) & 0) | msb ) | (image & 0xFE);
+
 		//int msgIndex = b.length / 8;
 		return image;
 	}
